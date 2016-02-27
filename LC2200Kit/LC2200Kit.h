a13a9f8c3ebb7efe6d6a7e338c968f8f2582f780
Jonathan Jemson <jonathanjemson@gatech.edu>
2016-02-27 11:49:18 -0500

    Add Carthage/Framework support

---
+++ LC2200Kit/LC2200Kit.h
@@ -0,0 +1,19 @@
+//
+//  LC2200Kit.h
+//  LC2200Kit
+//
+//  Created by Jonathan Jemson on 2/27/16.
+//  Copyright © 2016 Jonathan Jemson. All rights reserved.
+//
+
+@import Foundation;
+
+//! Project version number for LC2200Kit.
+FOUNDATION_EXPORT double LC2200KitVersionNumber;
+
+//! Project version string for LC2200Kit.
+FOUNDATION_EXPORT const unsigned char LC2200KitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LC2200Kit/PublicHeader.h>
+
+
